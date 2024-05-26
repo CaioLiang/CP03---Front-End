09cdfbb47e7cdb24f5ff791fb22a34c2cf4e57ef
Caio Liang <caioliang2@gmail.com>
2024-05-26 01:06:46 -0300

    Implementado estutura de adicionar na lista de tarefas

--- CP 3/CP 3/js/script.js
+++ CP 3/CP 3/js/script.js
@@ -1,9 +1,18 @@
-const form = document.querySelector("#todo-form")
+const Todoform = document.querySelector("#todo-form")
 const tarefa = document.querySelector("#new-task")
 
-console.log(form, tarefa)
-
 //evento
-form.addEventListener("submit", (event) => {
+Todoform.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    const valueTask = tarefa.value
+
+    const addDiv = document.createElement("div")
+    addDiv.classList.add("list-task")
+    
+    const addText = document.createElement("p")
+    addText.innerText = valueTask;
+    addDiv.appendChild(addText)
+
+    Todoform.appendChild(addDiv) 
 })

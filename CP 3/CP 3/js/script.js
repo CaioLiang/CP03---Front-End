69c27bc5aa2dfcc082c064cd62acfd0d03571832
Caio Liang <caioliang2@gmail.com>
2024-05-26 02:09:06 -0300

    Armazenando tarefas no localStorage e ajuste de cores

--- CP 3/CP 3/js/script.js
+++ CP 3/CP 3/js/script.js
@@ -1,18 +1,28 @@
 const Todoform = document.querySelector("#todo-form")
 const tarefa = document.querySelector("#new-task")
+var keyDelete = 0
 
-//evento
+
+
 Todoform.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const valueTask = tarefa.value
+    
+    keyDelete++
+    
+    var tarefaFormatado = keyDelete + ' - ' + tarefa.value
+    const valueTask = tarefaFormatado
 
     const addDiv = document.createElement("div")
     addDiv.classList.add("list-task")
     
-    const addText = document.createElement("p")
+    const addText = document.createElement("h3")
     addText.innerText = valueTask;
     addDiv.appendChild(addText)
-
     Todoform.appendChild(addDiv) 
+    
+    localStorage.setItem(keyDelete, tarefa.value)
+    
+    tarefa.value = ""
+    tarefa.focus();
+
 })

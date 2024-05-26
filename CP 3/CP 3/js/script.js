31d101688565f2b44a3931ef06d12d33397a6357
Caio Liang <caioliang2@gmail.com>
2024-05-26 19:18:19 -0300

    Adicionando botão de excluir com script sem integração com localStorage

--- CP 3/CP 3/js/script.js
+++ CP 3/CP 3/js/script.js
@@ -2,6 +2,7 @@ const Todoform = document.querySelector("#todo-form")
 const adicionarTarefa = document.querySelector("#new-task")
 var keyCount = 0
 
+//colocando os elementos do localStorage
  while (keyCount < localStorage.length) {
     keyCount++
     const reloadTask = localStorage.getItem(keyCount) 
@@ -17,6 +18,11 @@ var keyCount = 0
     addDiv.appendChild(addText)
     Todoform.appendChild(addDiv) 
 
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("button-remove")
+    doneBtn.type = 'button'
+    doneBtn.innerHTML = '<img src="./img/trash-can.png" class="button-remove" alt="ícone de lixeira">'
+    addDiv.appendChild(doneBtn)
 }
 
 Todoform.addEventListener("submit", (event) => {
@@ -24,6 +30,7 @@ Todoform.addEventListener("submit", (event) => {
     
     keyCount++
     
+    //criando elementos da lista de tarefas
     var adicionarTarefaFormatado = keyCount + ' - ' + adicionarTarefa.value
     const valueTask = adicionarTarefaFormatado
 
@@ -40,4 +47,19 @@ Todoform.addEventListener("submit", (event) => {
     adicionarTarefa.value = ""
     adicionarTarefa.focus();
 
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("button-remove")
+    doneBtn.type = 'button'
+    doneBtn.innerHTML = '<img src="./img/trash-can.png" class="button-remove" alt="ícone de lixeira">'
+    addDiv.appendChild(doneBtn)
+})
+
+
+document.addEventListener("click", (event) => {
+    const clickRemove = event.target
+    const divProxima  = clickRemove.closest("div")
+    
+    if(clickRemove.classList.contains("button-remove")){
+        divProxima.remove()
+    }
 })
